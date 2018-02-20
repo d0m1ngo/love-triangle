baf088a47bb32498e15f851c1c852a7328b8ab7c
Henadzi <zubarik.gena@tut.by>
2018-02-20 17:40:41 +0000

    done

--- src/index.js
+++ src/index.js
@@ -2,6 +2,29 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+module.exports = function  getLoveTrianglesCount(arr) {
+  let c = 0;
+  	for (let i = 0; i < arr.length; i++){
+      let fr;
+      let sc;
+      let third;
+      
+  		fr = arr[i];
+      
+  		if (fr != 'undefined'){
+  			sc = arr[fr-1];
+        
+  			if (sc != 'indefined'){
+  				third = arr[sc-1];
+  				if (sc !=i+1 && third === i+1  )
+          	
+            
+            c++;
+  			}
+  		}
+	}
+	
+ 
+  
+  	return c/3;
+}
